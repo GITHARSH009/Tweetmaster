9469571dd7ef87710714e87a880d99ad85ad172f
GITHARSH009 <123097001+GITHARSH009@users.noreply.github.com>
2024-03-11 19:33:38 +0530

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors=require('cors');
 const app=express()
 const dotenv=require('dotenv')
 dotenv.config();
-const port=process.env.Port
+const port=process.env.Port || 8002
 app.use(cors());
 app.use(express.json());
 require("./conn");
